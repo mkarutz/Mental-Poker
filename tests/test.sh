f7688b8bb5c68658665b12db6a8c6e2e8327c68c
Brandon <bsyiem@student.unimelb.edu.au>
2017-05-13 16:04:12 +1000

    still not working

--- tests/test.sh
+++ tests/test.sh
@@ -5,3 +5,7 @@ gnome-terminal -e "java -jar build/libs/mentalpoker-all-1.0-SNAPSHOT.jar"
 sleep 1
 gnome-terminal -e "java -jar build/libs/mentalpoker-all-1.0-SNAPSHOT.jar"
 sleep 1
+gnome-terminal -e "java -jar build/libs/mentalpoker-all-1.0-SNAPSHOT.jar"
+sleep 1
+gnome-terminal -e "java -jar build/libs/mentalpoker-all-1.0-SNAPSHOT.jar"
+sleep 1
